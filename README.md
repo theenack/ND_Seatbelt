0732b356c22f39d621a1dfc045b58e0ccb6aa58e
theenack <90981544+theenack@users.noreply.github.com>
2022-10-20 15:26:53 -0400

    Initial Upload

---
+++ README.md
@@ -0,0 +1,9 @@
+# Seatbelt
+
+Script for FiveM. Possible prevent death in a crash.
+
+Installing:
+
+1. Download zip or clone repo.
+2. Add folder to your fivem server. Remember delete -master.
+3. Add start seatbelt.
---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+client_scripts {
+	'client.lua',
+	'config.lua'
+}
+
+ui_page 'html/ui.html'
+
+files {
+	'html/*',
+	'html/img/seatbelt.png',
+	'html/sounds/*.ogg'
+}
+client_script "@Badger-Anticheat/acloader.lua"
